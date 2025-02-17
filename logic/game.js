96cd2901ccb1a50f7b4a9e667ef223969558e1fa
Yesser-Ben-Amor <yesserbenamor@yahoo.fr>
2025-02-17 23:04:50 +0100

    Add character display to game canvas

--- logic/game.js
+++ logic/game.js
@@ -1,11 +1,18 @@
 let canvas;
 let ctx;
+let character = new Image();
 
 function init() {
     canvas = document.getElementById('gameCanvas');
     ctx = canvas.getContext('2d');
+    character.src = 'views/img/1.Sharkie/1.IDLE/1.png';
     
-    // Game initialization code here
+    // Starte den Game Loop sobald das Bild geladen ist
+    character.onload = function() {
+        console.log('Character image loaded');
+        gameLoop();
+    }
+    
     console.log('Game initialized');
 }
 
@@ -14,9 +21,10 @@ function gameLoop() {
     // Clear canvas
     ctx.clearRect(0, 0, canvas.width, canvas.height);
     
-    // Update game state
-    
-    // Draw game objects
+    // Draw character in the center of canvas
+    let x = (canvas.width - character.width) / 2;
+    let y = (canvas.height - character.height) / 2;
+    ctx.drawImage(character, x, y);
     
     // Request next frame
     requestAnimationFrame(gameLoop);
