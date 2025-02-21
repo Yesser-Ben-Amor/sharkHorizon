17e14e853a0e6cec322de8a0a8a251221fb8bbb6
Yesser-Ben-Amor <yesserbenamor@yahoo.fr>
2025-02-21 14:36:22 +0100

    Refactor enemy image loading to use super.loadImage() for consistent OOP style

--- models/fisch.class.js
+++ models/fisch.class.js
@@ -1,10 +1,10 @@
 class Fisch extends Entity {
     constructor(x, y, größe, geschwindigkeit, punkte) {
         super(x, y);
+        super.loadImage('views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
         this.größe = größe;
         this.geschwindigkeit = geschwindigkeit;
         this.punkte = punkte;
-        this.loadImage('views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
     }
 
     schwimmen() {
