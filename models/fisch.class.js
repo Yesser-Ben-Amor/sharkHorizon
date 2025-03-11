63d96e5211e25050c611e413557812542461e887
Yesser-Ben-Amor <yesserbenamor@yahoo.fr>
2025-03-11 17:31:25 +0100

    Update Fisch class to use all swimming images for animation

--- models/fisch.class.js
+++ models/fisch.class.js
@@ -1,10 +1,39 @@
 class Fisch extends Entity {
+    IMAGES_SWIMMING_FISH = [
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim2.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim3.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim4.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim5.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim1.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim2.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim3.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim4.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/2.swim5.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim1.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim2.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim3.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim4.png',
+        'views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/3.swim5.png'
+    ];
+    currentImage = 0;
+
     constructor(x, y, größe, geschwindigkeit, punkte) {
         super(x, y);
         super.loadImage('views/img/2.Enemy/1.Puffer fish (3 color options)/1.Swim/1.swim1.png');
+        this.loadImageFromCache(this.IMAGES_SWIMMING_FISH);
+        this.animate();
         this.größe = größe;
         this.geschwindigkeit = geschwindigkeit;
         this.punkte = punkte;
+    }
+
+    animate() {
+        setInterval(() => {
+            this.currentImage = (this.currentImage + 1) % this.IMAGES_SWIMMING_FISH.length;
+            let path = this.IMAGES_SWIMMING_FISH[this.currentImage];
+            this.img = this.imageCache[path]; // Setze das aktuelle Bild
+        }, 100);
     }
 
     schwimmen() {
